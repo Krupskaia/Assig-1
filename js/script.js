2e4032a117c507d2af51b395b26df8b0606f7e59
KRUPSKAIA ANGULO <krupskaia@outlook.com>
2023-09-27 21:54:49 -0700

    final

---
+++ js/script.js
@@ -0,0 +1,123 @@
+t// STEP 1
+    /*let someMonth
+    function theMonth()
+    let currentMonth
+    let summerMonth
+    let myLibraryFunction*/
+
+
+// STEP 2
+    // numeric literal expression
+    /*5
+    //string literal expression
+    "Alexandra Angulo"
+    //Boolean literal expression
+    false
+    //null literal expression
+    null*/
+    
+// STEP 3
+
+var exp2 = 16 * (100/25) + 2;
+var exp2 = 10 + 5;
+
+// STEP 4
+
+var strFirstName;
+var strLastName;
+var strAddress;
+var strCity;
+var strState;
+var strZipCode;
+var intYourAge;
+var strReferralSource;
+var blnMayWeContactYou;
+
+// STEP 5
+
+var strFirstName;
+strFirstName = "Alexandra";
+var strLastName = "Angulo";
+var strAddress = "871 Street ", strCity = "San Diego";
+
+// STEP 6
+let streetAndNumber = "El Camino Real" + 871
+window.console.log(streetAndNumber);
+
+// STEP 7
+var isReady = true + "is ready";
+window.console.log(isReady);
+var complete = 100 + true;    
+window.console.log(complete);
+
+// STEP 8
+let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+console.log(someString)
+
+// STEP 9
+var x = null;
+window.console.log(x);
+var y;
+window.console.log(y);
+
+// STEP 10
+window.console.log(typeof "Alexandra");
+window.console.log(typeof 15.13);
+window.console.log(typeof true);
+window.console.log(typeof {lastName: "Angulo"});
+window.console.log(typeof myHouse);
+
+// STEP 11
+window.alert("Hello " + "Zak Ruvalcaba" + ", welcome to the JavaScript class!");
+
+// STEP 12
+/*  let name = "Alexandra Angulo";
+window.alert("Hello " + name + ", welcome to the JavaScript class!"); */
+
+// STEP 13
+/*     let name = "Alexandra Angulo";
+let course = "JavaScript";
+window.alert("Hello " + name + ", welcome to the " + course + " class!"); */
+
+// STEP 14
+/*     let name = "Alexandra Angulo";
+let course = "JavaScript";
+window.alert("Hello " + name + ". \nWelcome to the " + course + " class!"); */
+
+// STEP 15
+/*     let name = window.prompt("What is your name?");
+window.alert("Hello " + name + "."); */
+
+// STEP 16
+/*  let course = window.prompt("What is your course?");
+window.alert("Hello " + course + "."); */
+
+// STEP 17
+/*  let x = 10;
+let y = 20;
+window.console.log(x + y);*/
+
+// STEP 18
+/*  let x = 20;
+x += 20;
+window.console.log(x);*/
+
+// STEP 19
+/*  let x = 20;
+x *= 5;
+window.console.log(x);*/
+
+// STEP 20
+/*  let x = 20 % 3;
+x /= 1;
+window.console.log(x);*/
+
+// STEP 21
+/* let x = (15 > 4) || (3 > 10);
+window.console.log(x);*/
+
+// STEP 22
+/* let x = (4 < 8) && (100 < 10);
+window.console.log(x);*/
+
+
